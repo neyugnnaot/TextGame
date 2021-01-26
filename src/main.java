7ac5ad9b040de21bab612f93b3709f81a8c15dca
Toan <nguyen.toan.0397@gmail.com>
2021-01-25 20:02:01 -0500

    Updates to combat and addition of Maven

--- src/main.java
+++
@@ -1,9 +0,0 @@
-import com.textgame.gameloop.Game;
-
-public class main {
-
-    public static void main(String[] args) {
-        Game.start();
-        System.out.println("Thanks for playing!");
-    }
-}
